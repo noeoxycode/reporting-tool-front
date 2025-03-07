416f03ab996e15a7830a1081f56388ee79540912
noeoxycode <noe@marleix.com>
2025-03-07 14:25:39 +0100

    adviser entity img: WIP

--- src/app/adviser.form.tsx
+++ src/app/adviser.form.tsx
@@ -10,6 +10,7 @@ interface Adviser {
     first_name: string;
     last_name: string;
     representative_number: string;
+    image: File | null;
     address: string;
     email: string;
     mobile: string;
@@ -20,6 +21,7 @@ export default function AdviserForm() {
         first_name: '',
         last_name: '',
         representative_number: '',
+        image: null,
         address: '',
         email: '',
         mobile: '',
@@ -65,13 +67,25 @@ export default function AdviserForm() {
                 <CardContent>
                     <form onSubmit={handleSubmit} className="space-y-4">
                         <div className="grid grid-cols-2 gap-4">
-                            <LabelAndInput label={'First Name'} onChange={handleChange} value={formData.first_name} />
-                            <LabelAndInput label={'Last Name'} onChange={handleChange} value={formData.last_name} />
+                            <LabelAndInput label={'First Name'} onChange={handleChange} value={formData.first_name}/>
+                            <LabelAndInput label={'Last Name'} onChange={handleChange} value={formData.last_name}/>
                         </div>
-                        <LabelAndInput label={'Representative Number'} onChange={handleChange} value={formData.representative_number} />
-                        <LabelAndInput label={'Address'} onChange={handleChange} value={formData.address} />
-                        <LabelAndInput type="email" label={'Email'} onChange={handleChange} value={formData.email} />
-                        <LabelAndInput type="tel" label={'Mobile'} onChange={handleChange} value={formData.mobile} />
+                        <LabelAndInput label={'Representative Number'} onChange={handleChange}
+                                       value={formData.representative_number}/>
+                        <div className="flex flex-col">
+                            <label htmlFor="image" className="text-sm font-medium">Image</label>
+                            <input
+                                type="file"
+                                id="image"
+                                name="image"
+                                accept="image/*"
+                                onChange={handleChange}
+                                className="mt-2 border rounded-lg p-2"
+                            />
+                        </div>
+                        <LabelAndInput label={'Address'} onChange={handleChange} value={formData.address}/>
+                        <LabelAndInput type="email" label={'Email'} onChange={handleChange} value={formData.email}/>
+                        <LabelAndInput type="tel" label={'Mobile'} onChange={handleChange} value={formData.mobile}/>
                         <Button type="submit" className="w-full">Submit</Button>
                     </form>
                 </CardContent>
