3ed5d0170519210551cb141c9a61eebd6680ac9e
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2025-03-27 10:11:13 +0100

    [404 page]: update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,18 +6,18 @@ export default function NotFound() {
             <div className="max-w-lg w-full space-y-8 text-center">
                 <div>
                     <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
-                        Page non trouvée
+                        Page not found
                     </h2>
                     <p className="mt-2 text-sm text-gray-600">
-                        La page que vous recherchez n&apos;existe pas ou a été déplacée.
+                        The page you are looking for does not exist.
                     </p>
                 </div>
                 <div className="mt-8">
                     <Link
-                        href="/individuals"
+                        href="/"
                         className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                     >
-                        Retourner à la liste des individuals
+                        Back to home page
                     </Link>
                 </div>
             </div>
